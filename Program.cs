d6a8364fe1c6af9eb79554e9c282d044791a0999
André César Vieira <andrecesarvieira@hotmail.com>
2025-08-14 08:58:06 -0300

    update : desafio celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "000000000000001", 256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\niPhone:");
+Smartphone iphone = new Iphone("987654321", "iPhone 13", "000000000000002", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
